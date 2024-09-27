b7a80e1bd0811b25db7f8060f881fa2d2a620922
TomerNoy <mail2tomer@gmail.com>
2024-09-27 10:42:55 +0300

    content update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Articles from './pages/Articles';
-import Contact from './pages/Contact';
-import Cbt from './pages/Cbt';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
+import Contact from "./pages/Contact";
+import Cbt from "./pages/Cbt";
 
 const App = () => {
   return (
     <Router>
-      <div className='header-wrapper'>
+      <div className="header-wrapper">
         <Header />
       </div>
       <main>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,51 +1,60 @@
-import React from 'react';
-import './Home.css';
-import { Link, Element } from 'react-scroll';
+import React from "react";
+import "./Home.css";
+import { Element } from "react-scroll";
 
+import inbalImage from "../assets/inbal.jpeg";
 
 const Home = () => {
-    return <div>
-        {/* <nav>
-            <Link to="section1" smooth={true} duration={500}>Section 1</Link>
-            <Link to="section2" smooth={true} duration={500}>Section 2</Link>
-            <Link to="section3" smooth={true} duration={500}>Section 3</Link>
-        </nav> */}
-        <Element name="section1" className="section">
-            <div className='home-banner'>
-                <div className='home-banner-title1'>
-                    Dolor consectetur deserunt do et pariatur anim .
-                </div>
-                <div className='home-banner-title2'>
-                    Ullamco commodo labore magna laboris sunt tempor.
-                </div>
+  return (
+    <div>
+      <Element name="banner-section" className="section home-banner">
+        {/* <div id="top-banner-title">
+          Dolor consectetur deserunt do et pariatur anim .
+        </div> */}
+        <p id="bottom-banner-title">
+        טיפול CBT – איתך במסע לשינוי חשיבה, <br/>רגש והתנהגות בדרך לחיים טובים יותר
+        </p>
+      </Element>
+      <Element name="about-section" className="section">
+        <div className="home-about">
+          <h2>קצת עלי</h2>
+          <div className="about-row">
+            <img src={inbalImage} alt="Inbal"></img>
+            <div className="about-row-inner">
+              <p>
+                נעים להכיר, אני ענבל שאול, פסיכותרפיסטית קוגניטיבית-התנהגותית.
+              </p>
 
+              <p>
+                אני מטפלת ומלווה במצבי קושי ומשבר כמו חרדה, דיכאון, הפרעות
+                התנהגות שונות, התמודדות עם כעסים, תחושת ערך עצמי נמוך, תחושת
+                תקיעות, קושי בניהול מערכות יחסים ועוד. אני מטפלת בגישה
+                הקוגניטיבית התנהגותית, המתייחסת לקשר שבין מחשבות, רגשות
+                והתנהגות. אני מאמינה שבאמצעות הקשבה והתבוננות משותפת ללא
+                שיפוטיות אפשר לצמוח.
+              </p>
             </div>
-        </Element>
-        <Element name="section2" className="section">
-            <h2>Section 2</h2>
-        </Element>
-        <Element name="section3" className="section">
-            <h2>Section 3</h2>
-        </Element>
+          </div>
+        </div>
+      </Element>
+      <Element name="treatment-section" className="section">
+        <div className="home-treatments">
+          <h2>תחומי טיפול</h2>
+          <p>
+            טיפול פסיכולוגי קוגניטיבי התנהגותי מתמקד בקשר בין מחשבות, רגשות
+            והתנהגות. טיפול זה מתאים לאנשים המתמודדים עם קשיים כמו חרדה, דיכאון,
+            תחושת ערך עצמי נמוך, קשיים בניהול מערכות יחסים ועוד.
+          </p>
+          <p>
+            במהלך הטיפול אני מסייעת לך להבין את המחשבות והרגשות שלך, לזהות
+            תהליכים חשיבתיים שליליים ולפתור ביחד איתך קשיים ומשברים.
+          </p>
+        </div>
+      </Element>
 
+      
     </div>
-
-
-
-    // <div className="home-container">
-    //     <div className="home-content">
-
-    //         <div className='header1'>
-    //             Dolor consectetur deserunt do et pariatur anim eiusmod officia cillum exercitation.
-    //         </div>
-    //         <div className='header2'>
-    //             Ullamco commodo labore magna laboris sunt tempor ipsum pariatur incididunt et consectetur commodo in.
-    //         </div>
-
-    //     </div>
-
-
-    // </div>
+  );
 };
 
 export default Home;
