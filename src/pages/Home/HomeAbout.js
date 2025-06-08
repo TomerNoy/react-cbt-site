3078eefbbf6c497ad86a86d7027afbff16f22180
TomerNoy <mail2tomer@gmail.com>
2025-06-08 13:21:49 +0300

    modified index params, manifest and some text contect updates

--- src/pages/Home/HomeAbout.js
+++ src/pages/Home/HomeAbout.js
@@ -4,34 +4,27 @@ import inbalImage from "../../assets/inbal.avif";
 import inbalImageAlt from "../../assets/inbal.jpeg";
 import "./HomeAbout.css";
 
-const HomeAbout = () => {
-  return (
-    <Element name="about-section" className="section">
-      <div className="home-about page-text">
-        <h1>קצת עלי</h1>
-        <div className="about-row">
-          <picture>
-            <source srcSet={inbalImage} type="image/avif" />
-            <source srcSet={inbalImageAlt} type="image/jpeg" />
-            <img src={inbalImage} alt="Inbal" />
-          </picture>
+const HomeAbout = () => (
+  <Element name="about-section" className="section">
+    <div className="home-about page-text">
+      <h1>קצת עלי</h1>
+      <div className="about-row">
+        <picture>
+          <source srcSet={inbalImage} type="image/avif" />
+          <source srcSet={inbalImageAlt} type="image/jpeg" />
+          <img src={inbalImage} alt="Inbal" />
+        </picture>
 
-          <div className="about-row-inner">
-            <p>
-              נעים להכיר, אני ענבל שאול, פסיכותרפיסטית קוגניטיבית-התנהגותית. אם
-              הגעת לכאן, כנראה שיש בך רצון לשנות משהו – והנה, כבר עשית את הצעד
-              הראשון לקראת השינוי… <br />
-              אני מטפלת בגישה הקוגניטיבית-התנהגותית, שמתמקדת בקשר בין מחשבות,
-              רגשות והתנהגויות. במהלך הטיפול, אני כאן כדי לתמוך, להכווין וללוות
-              אותך בהתמודדות עם השינוי הרצוי. אני מאמינה שלכל אחד ואחת מגיעה
-              הזכות לחיות חיים מספקים, ודרך התבוננות משותפת והכוונה מדויקת, נוכל
-              להגיע לשיפור משמעותי ולהשגת היעדים שחשובים לך.
-            </p>
-          </div>
+        <div className="about-row-inner">
+          <p>
+            נעים להכיר, אני ענבל שאול, פסיכותרפיסטית קוגניטיבית-התנהגותית. אם הגעת לכאן, כנראה שיש בך רצון לשנות משהו – והנה, כבר עשית את הצעד הראשון לקראת השינוי…
+            <br />
+            אני מטפלת בגישה הקוגניטיבית-התנהגותית, שמתמקדת בקשר בין מחשבות, רגשות והתנהגויות. בטיפול אני מציעה מרחב שבו נוכל לעצור יחד, להקשיב למה שמתרחש בתוכך, ולחקור את המחשבות והתחושות שמובילות אותך ביומיום. נבין יחד דפוסי חשיבה והתנהגות, ונחקור דרכים חדשות שמתאימות לך. בתוך התהליך הזה, לאט ובהדרגה, נוכל להתחבר לכוחות שלך – ולצעוד לעבר שינוי והתפתחות.
+          </p>
         </div>
       </div>
-    </Element>
-  );
-};
+    </div>
+  </Element>
+);
 
 export default HomeAbout;
